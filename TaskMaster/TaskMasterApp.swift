fb8a36428d120c40d0ee3dd90516ea8d02e33eed
Nikhil Singh <nikhil.singh@xyb.co>
2026-02-08 00:33:20 +0200

    Initial Commit

---
+++ TaskMaster/TaskMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMasterApp.swift
+//  TaskMaster
+//
+//  Created by Nikhil Singh on 08.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct TaskMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
